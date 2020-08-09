04c0f6d1537cb04cb0c799ae6043c16dae01f834
Javier Aguilar <coyote176@msn.com>
2020-08-09 02:28:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,7 +118,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay][:known_for] = alans_new_info = "GUI"
 
      programmer_hash
      #programmer_hash[:alan_kay][:known_for] = alans_new_info = "GUI"
